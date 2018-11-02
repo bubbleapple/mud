60d44af37e699e2a4768ff997e9c1a56787c11f0
Lei Ju <mrleiju@users.noreply.github.com>
2018-11-02 09:07:23 -0400

    init

---
+++ ClientThread.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.net.*;
+
+public class ClientThread extends Thread {
+    protected Socket socket;
+
+    public ClientThread(Socket clientSocket) {
+        this.socket = clientSocket;
+    }
+
+    public void run() {
+        InputStream inp = null;
+        BufferedReader brinp = null;
+        DataOutputStream out = null;
+        try {
+            inp = socket.getInputStream();
+            // from javadoc: 
+            // In general, each read request made of a Reader causes a
+            // corresponding read request to be made of the underlying
+            // character or byte stream.  It is therefore advisable to wrap
+            // a BufferedReader around any Reader whose read() operations
+            // may be costly, such as FileReaders and InputStreamReaders.
+            brinp = new BufferedReader(new InputStreamReader(inp));
+            out = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            return;
+        }
+        String line;
+        try {
+        	out.writeChars("\n\nWelcome to the new world!!\n\n");
+        } catch(IOException e) {
+        	e.printStackTrace();
+            return;
+        }
+        while (true) {
+            try {
+                line = brinp.readLine();
+                if ((line == null) || line.equalsIgnoreCase("QUIT")) {
+                    socket.close();
+                    return;
+                } else {
+                    out.writeBytes(line + "\n\r");
+                    out.flush();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+        }
+    }
+}
---
+++ MudServer.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.net.*;
+
+
+public class MudServer {
+
+    static final int PORT = 9330;
+
+    public static void main(String args[]) {
+        ServerSocket serverSocket = null;
+        Socket socket = null;
+
+        try {
+            serverSocket = new ServerSocket(PORT);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+        while (true) {
+            try {
+                socket = serverSocket.accept();
+            } catch (IOException e) {
+                System.out.println("I/O error: " + e);
+            }
+            // new thread for a client
+            new ClientThread(socket).start();
+        }
+    }
+}
